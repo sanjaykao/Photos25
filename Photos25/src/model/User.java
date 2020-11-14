17f8a0b6286b1b30483d92b6e948d4df2503365e
Virginia Cheng <vcheng328@gmail.com>
2020-11-14 01:56:37 -0500

    feat: filled in some User methods

--- Photos25/src/model/User.java
+++ Photos25/src/model/User.java
@@ -23,28 +23,37 @@ public class User implements Serializable{
 	}
 	
 	public void createAlbum(String name) {
-		//create new album in the user home page
+		//create new album in the user home page then add to the arraylist of albums
+		Album newAlbum = new Album(name);
+		albums.add(newAlbum);
 		
 	}
 	
-	public void createAlbum(ArrayList<Photo> pics) {
-		//create new album from the search of photocs
-		
+	public void createAlbum(String name, ArrayList<Photo> pics) {
+		//create new album from the search of photos and then add to the arrlist of albums
+		Album newAlbum = new Album(name, pics);
+		albums.add(newAlbum);
 	}
 	
 	public void deleteAlbum(String name) {
 		//removes album by name from the albums arraylist
-		
+		String indexName;
+		for(int i = 0; i < albums.size(); i++) {
+			indexName = albums.get(i).getAlbumName();
+			if(indexName.equals(name)) {
+				albums.remove(i);
+			}
+		}
 	}
 	
-	public void renameAlbum(String newName) {
+	public void renameAlbum(Album album, String newName) {
 		//sets new name of album 
-		
+		album.setAlbumName(newName);
 	}
 
 	public void addCaption(String content, Photo photo) {
 		//adds or edits the caption to the photo
-		
+		photo.setCaption(content);
 	}
 	
 	public void displayPhoto(Photo photo) {
@@ -57,21 +66,36 @@ public class User implements Serializable{
 	
 	public void addTag(Photo photo, String name, String value) {
 		//creates a new tag and then adds to the photo's tag arraylist
-		
+		ArrayList<Tag> tags = photo.getTags();
+		Tag newTag = new Tag(name, value);
+		tags.add(newTag);
 	}
 	
 	public void deleteTag(Photo photo, Tag tag) {
 		//finds tag in the arraylist of tags and removes it
+		ArrayList<Tag> tags = photo.getTags();
+		for(int i = 0; i < tags.size(); i++) {
+			if(tags.get(i).equals(tag)) {
+				tags.remove(i);
+			}
+		}
+		
 	}
 	
 	public void copyPhoto(Album dest, Photo photo) {
 		//creates a copy of the photo and adds to the dest album's arraylist of photos
 		//also update the photo's arraylist of albums
+		dest.getPhotos().add(photo);
+		photo.getAlbums().add(dest);
 	}
 	
 	public void movePhoto(Album dest, Album source, Photo photo) {
 		//removes photo from source and add to the dest album's arraylist of photos
 		//also update the photo's arralist of albums
+		dest.getPhotos().add(photo);
+		photo.getAlbums().add(dest);
+		photo.getAlbums().remove(source);
+		source.getPhotos().remove(photo);
 	}
 	
 	public ArrayList<Photo> searchByDate(Date first, Date last) {
